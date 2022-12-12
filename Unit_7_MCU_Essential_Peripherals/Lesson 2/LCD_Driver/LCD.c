757253ad4343537c0d65b01b54a186a48544c38d
Abdullah Kortam <93476335+Abdullahkortam@users.noreply.github.com>
2022-12-12 12:31:56 +0200

    Unit_7_MCU_Essential_Peripherals
    Unit_7_MCU_Essential_Peripherals

---
+++ Unit_7_MCU_Essential_Peripherals/Lesson 2/LCD_Driver/LCD.c
@@ -0,0 +1,170 @@
+﻿/*
+ * LCD.c
+ *
+ * Created: 04/11/2022 08:08:21 م
+ *  Author: Abdullah Kortam
+ */ 
+#include "LCD.h"
+void LCD_KICK()
+{
+	LCD_CTRL &= ~(1<<EN_PIN);
+	_delay_ms(30);
+	LCD_CTRL |= (1<<EN_PIN);
+}
+
+void LCD_GOTO(int line , int position)
+{
+	if(line == 1)
+	{
+		if(position <16 && position >=0)
+		LCD_WRITE_COMMAND(LCD_CMD_BEGIN_AT_FIRST_ROW+position);
+	}
+	else if(line == 2)
+	{
+		if(position <16 && position >=0)
+		LCD_WRITE_COMMAND(LCD_CMD_BEGIN_AT_SECOND_ROW+position);
+	}
+	else if(line == 3)
+	{
+		if(position <16 && position >=0)
+		LCD_WRITE_COMMAND(LCD_CMD_BEGIN_AT_THIRD_ROW+position);
+	}
+	else if(line == 4)
+	{
+		if(position <16 && position >=0)
+		LCD_WRITE_COMMAND(LCD_CMD_BEGIN_AT_fourth_ROW+position);
+	}
+}
+void LCD_Clear_Screen(void)
+{
+	LCD_WRITE_COMMAND(LCD_CMD_CLEAR_SCREEN);
+}
+void LCD_isbusy()
+{
+	_delay_ms(20);
+	DataDir_LCD_PORT &= ~ (0xFF << DATA_SHIFT) ;
+	LCD_CTRL |= (1<<RW_PIN) ;
+	LCD_CTRL &= ~(1<<RS_PIN) ;
+	LCD_KICK();
+	DataDir_LCD_PORT = 0xFF ;
+	LCD_CTRL &= ~(1<<RW_PIN);
+}
+void LCD_INIT()
+{
+	_delay_ms(20);
+	LCD_isbusy();
+	DataDir_LCD_CTRL |= (1<<RS_PIN) | (1<<RW_PIN) | (1<<EN_PIN) ;
+	LCD_CTRL &= ~ (1<<RS_PIN) | (1<<RW_PIN) | (1<<EN_PIN) ;
+	DataDir_LCD_PORT = 0xFF ;
+	_delay_ms(15);
+	LCD_Clear_Screen();
+	
+	#ifdef EIGHT_BIT_MODE
+		LCD_WRITE_COMMAND(LCD_CMD_FUNCTION_8BIT_2LINES);
+	#endif
+	
+	#ifdef FOUR_BIT_MODE
+		LCD_WRITE_COMMAND(0x02);
+		LCD_WRITE_COMMAND(LCD_CMD_FUNCTION_4BIT_2LINES);
+	#endif
+	
+	LCD_WRITE_COMMAND(LCD_CMD_ENTRY_MODE);
+	LCD_WRITE_COMMAND(LCD_CMD_BEGIN_AT_FIRST_ROW);
+	LCD_WRITE_COMMAND(LCD_CMD_DISP_ON_CURSOR_BLINK);
+	
+}
+
+void LCD_WRITE_COMMAND(unsigned char command)
+{
+	#ifdef EIGHT_BIT_MODE
+		LCD_isbusy();
+		LCD_PORT = command ;
+		LCD_CTRL &= ~(1<<RW_PIN);
+		LCD_CTRL &= ~(1<<RS_PIN);
+		_delay_ms(1);
+		LCD_KICK();
+	#endif
+	
+	#ifdef  FOUR_BIT_MODE
+		LCD_isbusy();
+		LCD_PORT = (LCD_PORT & 0x0F) | (command & 0xF0) ;
+		LCD_CTRL &= ~ ((1<<RW_PIN) | (1<<RS_PIN)) ;
+		_delay_ms(1);
+		LCD_KICK() ;
+		LCD_PORT = (LCD_PORT & 0x0F) | (command<<4)  ;
+		LCD_CTRL &= ~ ((1<<RW_PIN) | (1<<RS_PIN)) ;
+		_delay_ms(1);
+		LCD_KICK() ;
+	#endif
+	
+	
+}
+
+void LCD_WRITE_CHAR(unsigned char data)
+{
+	#ifdef EIGHT_BIT_MODE
+		LCD_isbusy();
+		LCD_CTRL |=  (1<<RS_PIN);
+		LCD_PORT = data ;
+		LCD_CTRL &= ~(1<<RW_PIN);
+		LCD_CTRL |=  (1<<RS_PIN);
+		LCD_KICK();
+	#endif
+	
+	#ifdef FOUR_BIT_MODE
+		LCD_PORT = (LCD_PORT & 0x0F) | (data & 0xF0) ;
+		LCD_CTRL &= ~(1<<RW_PIN) ;
+		LCD_CTRL |= (1<<RS_PIN);
+		LCD_KICK();
+		_delay_ms(1);
+		LCD_PORT = (LCD_PORT & 0x0F) | (data<<4); 
+		LCD_CTRL &= ~(1<<RW_PIN) ;
+		LCD_CTRL |= (1<<RS_PIN);
+		_delay_ms(1);
+		LCD_KICK();
+		
+	#endif
+}
+
+void LCD_WRITE_STRING(char* data)
+{
+	int count =0 ;
+	while(*data >0)
+	{
+		count++ ;
+		LCD_WRITE_CHAR(*data++);
+		if (count==16)
+		{
+			LCD_GOTO(2,0);
+		}
+		else if(count ==32)
+		{
+			LCD_GOTO(3,0);
+		}
+		else if(count ==48)
+		{
+			LCD_GOTO(4,0);
+		}
+		else if(count ==64)
+		{
+			LCD_Clear_Screen();
+			LCD_GOTO(1,0);
+			count =0 ;
+		}
+	}
+	
+}
+void LCD_Interface(char r)
+{
+	switch(r)
+	{
+		case('?'):
+		LCD_Clear_Screen();
+		break;
+		case 'N':
+		break;
+		default:
+		LCD_WRITE_CHAR(r);
+		break;
+	}
+}
---
+++ Unit_7_MCU_Essential_Peripherals/Lesson 2/LCD_Driver/LCD.h
@@ -0,0 +1,61 @@
+﻿/*
+ * LCD.h
+ *
+ * Created: 04/11/2022 08:08:09 م
+ *  Author: Abdullah Kortam
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+#include <avr/io.h>
+#include <util/delay.h>
+
+//CLock
+#define F_CPU 1000000UL
+//Modes
+//#define EIGHT_BIT_MODE
+#define FOUR_BIT_MODE
+
+#define LCD_PORT PORTA
+#define DataDir_LCD_PORT DDRA
+#define LCD_CTRL PORTA
+#define DataDir_LCD_CTRL DDRA
+
+#define RS_PIN 1
+#define RW_PIN 2
+#define EN_PIN 3
+#define DATA_SHIFT 0
+
+// Commands of LCD
+#define LCD_CMD_FUNCTION_8BIT_2LINES   					(0x38)
+#define LCD_CMD_FUNCTION_4BIT_2LINES   					(0x28)
+#define LCD_CMD_MOVE_DISP_RIGHT       					(0x1C)
+#define LCD_CMD_MOVE_DISP_LEFT   						(0x18)
+#define LCD_CMD_MOVE_CURSOR_RIGHT   					(0x14)
+#define LCD_CMD_MOVE_CURSOR_LEFT 	  					(0x10)
+#define LCD_CMD_DISP_OFF   								(0x08)
+#define LCD_CMD_DISP_ON_CURSOR   						(0x0E)
+#define LCD_CMD_DISP_ON_CURSOR_BLINK   					(0x0F)
+#define LCD_CMD_DISP_ON_BLINK   						(0x0D)
+#define LCD_CMD_DISP_ON   								(0x0C)
+#define LCD_CMD_ENTRY_DEC   							(0x04)
+#define LCD_CMD_ENTRY_DEC_SHIFT   						(0x05)
+#define LCD_CMD_ENTRY_INC_   							(0x06)
+#define LCD_CMD_ENTRY_INC_SHIFT   						(0x07)
+#define LCD_CMD_BEGIN_AT_FIRST_ROW						(0x80)
+#define LCD_CMD_BEGIN_AT_SECOND_ROW						(0xC0)
+#define LCD_CMD_BEGIN_AT_THIRD_ROW						(0x90)
+#define LCD_CMD_BEGIN_AT_fourth_ROW						(0xD0)
+#define LCD_CMD_CLEAR_SCREEN							(0x01)
+#define LCD_CMD_ENTRY_MODE								(0x06)
+
+void LCD_INIT(void);
+void LCD_WRITE_COMMAND(unsigned char command);
+void LCD_WRITE_CHAR(unsigned char data);
+void LCD_WRITE_STRING(char* data);
+void LCD_isbusy(void);
+void LCD_Clear_Screen(void);
+void LCD_GOTO(int line , int position);
+void LCD_Interface(char r);
+#endif /* LCD_H_ */
